d027c3ef064f1855b8cf7c75314b5ab9c86b399e
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-09-25 13:38:54 +1000

    fixed vi path ref

--- link_and_vim.sh
+++ link_and_vim.sh
@@ -1,6 +1,6 @@
 ln -sf ~/projects/config/.bashrc ~/.bashrc
 ln -sf ~/projects/config/.bash_profile ~/.bash_profile
 ln -sf ~/projects/config/.vim/.vimrc ~/.vimrc
-sudo mkdir -p ~/.vim/bundle
-sudo git clone https://github.com/gmarik/vundle.git /.vim/bundle/vundle
+mkdir -p ~/.vim/bundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
